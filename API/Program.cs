114335dcb04a66ab956f32b89f3eb1f7d85de084
samratkar1-admin <frk7xg2pjw@privaterelay.appleid.com>
2025-06-25 14:40:18 -0500

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,10 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200", "http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
